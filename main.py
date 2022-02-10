a3a95459d4aecc63b175e9a3ecf5fd0340215b1f
Joel Valerian <joelvalerian@gmail.com>
2022-02-10 14:13:32 +0700

    Update main.py
    Implement cursor hover view on matplotlib

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ import pydicom
 import numpy as np
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
-import csv
-import cv2 as cv
-import math
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 class DoseDistanceExtractor:
 
@@ -67,28 +66,105 @@ class DoseDistanceExtractor:
 
     def plot(self):
         try:
-            self.source_ax1.imshow(self.rtdose[50])
-            self.source_ax2.imshow(self.rtdose[70])
-            self.source_canvas = FigureCanvasTkAgg(self.source_fig, self.plot_window)
-            self.source_canvas.draw()
-            self.source_canvas.get_tk_widget().grid(column=0, row=0)
-            self.target_ax1.imshow(self.rtdose[100])
-            self.target_ax2.imshow(self.rtdose[120])
-            self.target_canvas = FigureCanvasTkAgg(self.target_fig, self.plot_window)
-            self.target_canvas.draw()
-            self.target_canvas.get_tk_widget().grid(column=1, row=0)
-            self.plot_status.set('Plotted!')
+            self.source_ax1_plot = self.source_ax1.imshow(self.rtdose[120])
+            self.source_ax2_plot = self.source_ax2.imshow(self.rtdose[70])
+            self.target_ax1_plot = self.target_ax1.imshow(self.rtdose[100])
+            self.target_ax2_plot = self.target_ax2.imshow(self.rtdose[120])
 
+            self.source_fig1_canvas = FigureCanvasTkAgg(self.source_fig1, self.plot_window)
+            self.source_fig1_canvas.get_tk_widget().grid(column=0, row=0)
+            self.source_fig2_canvas = FigureCanvasTkAgg(self.source_fig2, self.plot_window)
+            self.source_fig2_canvas.get_tk_widget().grid(column=0, row=1)
+            self.target_fig1_canvas = FigureCanvasTkAgg(self.target_fig1, self.plot_window)
+            self.target_fig1_canvas.get_tk_widget().grid(column=1, row=0)
+            self.target_fig2_canvas = FigureCanvasTkAgg(self.target_fig2, self.plot_window)
+            self.target_fig2_canvas.get_tk_widget().grid(column=1, row=1)
+
+            self.source_fig1_divider = make_axes_locatable(self.source_ax1)
+            self.source_fig1_cb = self.source_fig1.colorbar(self.source_ax1_plot,
+                cax=self.source_fig1_divider.append_axes('right', size='5%', pad=0.05))
+            self.source_fig2_divider = make_axes_locatable(self.source_ax2)
+            self.source_fig2_cb = self.source_fig2.colorbar(self.source_ax2_plot,
+                cax=self.source_fig2_divider.append_axes('right', size='5%', pad=0.05))
+            self.target_fig1_divider = make_axes_locatable(self.target_ax1)
+            self.target_fig1_cb = self.target_fig1.colorbar(self.target_ax1_plot,
+                cax=self.target_fig1_divider.append_axes('right', size='5%', pad=0.05))
+            self.target_fig2_divider = make_axes_locatable(self.target_ax2)
+            self.target_fig2_cb = self.target_fig2.colorbar(self.target_ax2_plot,
+                cax=self.target_fig2_divider.append_axes('right', size='5%', pad=0.05))
+
+            self.plot_status.set('Plotted!')
+            print(np.argmax(self.rtdose[120]))
         except AttributeError:
             self.plot_status.set('No DICOM imported!')
+
+    def hover_source_ax1(self, event):
+        if event.inaxes == self.source_ax1:
+            x, y = event.xdata, event.ydata
+            i, j = np.round(np.array((x, y))).astype(int)
+            try:
+                self.plot_coord.set(str([round(x, 3), round(y, 4)]))
+                self.plot_index.set(str([i, j]))
+                self.plot_value.set(str(round(self.rtdose[50][j][i], 3)))
+            except AttributeError:
+                self.plot_status.set('No DICOM imported!')
+        else:
+            self.plot_coord.set(str(''))
+            self.plot_index.set(str(''))
+            self.plot_value.set(str(''))
+
+    def hover_source_ax2(self, event):
+        if event.inaxes == self.source_ax2:
+            x, y = event.xdata, event.ydata
+            i, j = np.round(np.array((x, y))).astype(int)
+            try:
+                self.plot_coord.set(str([round(x, 3), round(y, 4)]))
+                self.plot_index.set(str([i, j]))
+                self.plot_value.set(str(round(self.rtdose[70][j][i], 3)))
+            except AttributeError:
+                self.plot_status.set('No DICOM imported!')
+        else:
+            self.plot_coord.set(str(''))
+            self.plot_index.set(str(''))
+            self.plot_value.set(str(''))
+
+    def hover_target_ax1(self, event):
+        if event.inaxes == self.target_ax1:
+            x, y = event.xdata, event.ydata
+            i, j = np.round(np.array((x, y))).astype(int)
+            try:
+                self.plot_coord.set(str([round(x, 3), round(y, 4)]))
+                self.plot_index.set(str([i, j]))
+                self.plot_value.set(str(round(self.rtdose[100][j][i], 3)))
+            except AttributeError:
+                self.plot_status.set('No DICOM imported!')
+        else:
+            self.plot_coord.set(str(''))
+            self.plot_index.set(str(''))
+            self.plot_value.set(str(''))
+
+    def hover_target_ax2(self, event):
+        if event.inaxes == self.target_ax2:
+            x, y = event.xdata, event.ydata
+            i, j = np.round(np.array((x, y))).astype(int)
+            try:
+                self.plot_coord.set(str([round(x, 3), round(y, 4)]))
+                self.plot_index.set(str([i, j]))
+                self.plot_value.set(str(round(self.rtdose[120][j][i], 3)))
+            except AttributeError:
+                self.plot_status.set('No DICOM imported!')
+        else:
+            self.plot_coord.set(str(''))
+            self.plot_index.set(str(''))
+            self.plot_value.set(str(''))
 
     def __init__(self, root):
         self.root = root
         self.root.title('Dose Distance Extractor')
 
-        self.mainframe = tk.Frame(self.root, bd=0)
-        self.mainframe.grid(column=0, row=0)
-        
+        self.mainframe = tk.Frame(self.root)
+        self.mainframe.grid(column=0, row=0, sticky='nsew')
+
         self.main_title = tk.Label(self.mainframe, text='Dose Distance Extractor by Joel Valerian')
         self.main_title.grid(column=0, row=0)
         self.body = tk.Frame(self.mainframe)
@@ -179,24 +255,48 @@ class DoseDistanceExtractor:
         self.information_structure_list['yscrollcommand'] = self.structure_list_scrollbar.set
         
         ttk.Label(self.content, text='Dose Distribution').grid(column=0, row=0)
+        self.legend_window = tk.Frame(self.content)
+        self.legend_window.grid(column=0, row=1)
         self.plot_window = tk.Frame(self.content)
-        self.plot_window.grid(column=0, row=1)
+        self.plot_window.grid(column=0, row=2)
 
-        self.source_fig = Figure(figsize=(4, 4))
-        self.source_ax1 = self.source_fig.add_subplot(211)
-        self.source_ax2 = self.source_fig.add_subplot(212)
+        self.plot_coord = tk.StringVar()
+        self.plot_index = tk.StringVar()
+        self.plot_value = tk.StringVar()
+        self.plot_scale = 2
 
-        self.target_fig = Figure(figsize=(4, 4))
-        self.target_ax1 = self.target_fig.add_subplot(211)
-        self.target_ax2 = self.target_fig.add_subplot(212)
+        self.source_fig1 = Figure(figsize=(2.5*self.plot_scale, self.plot_scale))
+        self.source_ax1 = self.source_fig1.add_subplot()
+        self.source_fig2 = Figure(figsize=(2.5*self.plot_scale, self.plot_scale))
+        self.source_ax2 = self.source_fig2.add_subplot()
+        self.target_fig1 = Figure(figsize=(2.5*self.plot_scale, self.plot_scale))
+        self.target_ax1 = self.target_fig1.add_subplot()
+        self.target_fig2 = Figure(figsize=(2.5*self.plot_scale, self.plot_scale))
+        self.target_ax2 = self.target_fig2.add_subplot()
 
-        self.source_canvas = FigureCanvasTkAgg(self.source_fig, self.plot_window)
-        self.source_canvas.draw()
-        self.source_canvas.get_tk_widget().grid(column=0, row=0)
+        self.source_fig1_canvas = FigureCanvasTkAgg(self.source_fig1, self.plot_window)
+        self.source_fig1_canvas.get_tk_widget().grid(column=0, row=0)
+        self.source_fig2_canvas = FigureCanvasTkAgg(self.source_fig2, self.plot_window)
+        self.source_fig2_canvas.get_tk_widget().grid(column=0, row=1)
+        self.target_fig1_canvas = FigureCanvasTkAgg(self.target_fig1, self.plot_window)
+        self.target_fig1_canvas.get_tk_widget().grid(column=1, row=0)
+        self.target_fig2_canvas = FigureCanvasTkAgg(self.target_fig2, self.plot_window)
+        self.target_fig2_canvas.get_tk_widget().grid(column=1, row=1)
 
-        self.target_canvas = FigureCanvasTkAgg(self.target_fig, self.plot_window)
-        self.target_canvas.draw()
-        self.target_canvas.get_tk_widget().grid(column=1, row=0)
+        tk.Label(self.legend_window, text='Coordinates [X, Y]', width=20).grid(column=0, row=0)
+        tk.Label(self.legend_window, text='Indexes [i, j]', width=20).grid(column=1, row=0)
+        tk.Label(self.legend_window, text='Values (Gy)', width=20).grid(column=2, row=0)
+        self.plot_coord_label = tk.Label(self.legend_window, textvariable=self.plot_coord, width=10)
+        self.plot_coord_label.grid(column=0, row=1, sticky='nsew')
+        self.plot_index_label = tk.Label(self.legend_window, textvariable=self.plot_index, width=10)
+        self.plot_index_label.grid(column=1, row=1, sticky='nsew')
+        self.plot_value_label = tk.Label(self.legend_window, textvariable=self.plot_value, width=10)
+        self.plot_value_label.grid(column=2, row=1, sticky='nsew')
+
+        self.source_fig1_canvas.mpl_connect('motion_notify_event', self.hover_source_ax1)
+        self.source_fig2_canvas.mpl_connect('motion_notify_event', self.hover_source_ax2)
+        self.target_fig1_canvas.mpl_connect('motion_notify_event', self.hover_target_ax1)
+        self.target_fig2_canvas.mpl_connect('motion_notify_event', self.hover_target_ax2)
 
 if __name__ == '__main__':
     root = tk.Tk()
